36706e91532feb96231177c4e4d0fa83e31e4267
jomurielm <144398722+jomurielm@users.noreply.github.com>
2023-10-09 09:12:50 -0500

    Entrega definitiva del laboratorio propuesto.

--- pregunta.py
+++ pregunta.py
@@ -14,10 +14,12 @@ def clean_data():
     df = pd.read_csv("solicitudes_credito.csv", sep=";",index_col=0)
 
     df.dropna(inplace=True)
+
     df = df.apply(lambda x: x.astype(str).str.replace("_","-").str.replace("-"," ").str.replace("$","").str.replace(",",""))
     df = df.apply(lambda x: x.astype(str).str.lower().str.strip(" "))
     df.fecha_de_beneficio = pd.to_datetime(df["fecha_de_beneficio"],format='mixed', dayfirst = True)
     df.monto_del_credito = df.monto_del_credito.astype(float)
+    
     df.drop_duplicates(inplace=True)
 
     return df
